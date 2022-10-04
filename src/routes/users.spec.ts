d524daac2661e76168291acdf6bd4c73ecff12f8
Vadim <vadimpishprivate@gmail.com>
2022-10-04 09:14:52 -0700

    added post test route

--- src/routes/users.spec.ts
+++ src/routes/users.spec.ts
@@ -1,20 +1,30 @@
 import { describe, expect, it } from "vitest";
-import { dbConnect } from "../dbConnect";
-import { i } from "vitest/dist/index-6e18a03a";
-const request = require("supertest");
+import supertest from "supertest";
 import { app } from "../index";
-// const app = require("../index");
+
 describe("Test User Endpoint", () => {
-  it("tests GET /api/user endpoints", async () => {
-    const response = await request(app).get(
-      "/api/user/xSJzkd4sgkVCy5MygaDbDib7CCo1"
-    );
-    expect(response.body).toEqual({
-      _id: "632a3986939329bde8ea9eae",
+  it("tests POST /api/user endpoint", async () => {
+    const data = {
       firebase_id: "xSJzkd4sgkVCy5MygaDbDib7CCo1",
       email: "user@user.com",
       first_name: "Vadim",
-      __v: 0,
+    };
+    const response = await supertest(app).post("/api/user").send(data);
+    expect(response.body).toContain({
+      firebase_id: "xSJzkd4sgkVCy5MygaDbDib7CCo1",
+      email: "user@user.com",
+      first_name: "Vadim",
+    });
+    expect(response.statusCode).toBe(200);
+  });
+  it("tests GET /api/user endpoint", async () => {
+    const response = await supertest(app).get(
+      "/api/user/xSJzkd4sgkVCy5MygaDbDib7CCo1"
+    );
+    expect(response.body).toContain({
+      firebase_id: "xSJzkd4sgkVCy5MygaDbDib7CCo1",
+      email: "user@user.com",
+      first_name: "Vadim",
     });
     expect(response.statusCode).toBe(200);
   });
