811ef17a5f804f22f533f13c64fc8fde01f630e3
Vadim <vadimpishprivate@gmail.com>
2022-10-04 09:24:51 -0700

    refactored tests

--- tests/routes/users.spec.ts
+++ tests/routes/users.spec.ts
@@ -1,9 +1,9 @@
 import { describe, expect, it } from "vitest";
 import supertest from "supertest";
-import { app } from "../index";
+import { app } from "../../src/index";
 
 describe("Test User Endpoint", () => {
-  it("tests POST /api/user endpoint", async () => {
+  it("tests creating user", async () => {
     const data = {
       firebase_id: "xSJzkd4sgkVCy5MygaDbDib7CCo1",
       email: "user@user.com",
@@ -17,7 +17,20 @@ describe("Test User Endpoint", () => {
     });
     expect(response.statusCode).toBe(201);
   });
-  it("tests GET /api/user endpoint", async () => {
+  it("tests proper exception if creating user with duplicate ID", async () => {
+    const data = {
+      firebase_id: "xSJzkd4sgkVCy5MygaDbDib7CCo1",
+      email: "user@user.com",
+      first_name: "Vadim",
+    };
+    const response = await supertest(app).post("/api/user").send(data);
+    expect(response.body).toEqual({
+      error: true,
+      message: "Duplicate unique index passed, check firebase_id, or email",
+    });
+    expect(response.statusCode).toBe(400);
+  });
+  it("tests retreiving single user", async () => {
     const response = await supertest(app).get(
       "/api/user/xSJzkd4sgkVCy5MygaDbDib7CCo1"
     );
